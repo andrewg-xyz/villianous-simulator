87b68d3ea907e604d0a838d72fef714ca28e974e
andrewg-xyz <andrew@defenseunicorns.com>
2023-02-05 09:46:16 -0700

    gain power regex

--- src/internal/engine.go
+++ src/internal/engine.go
@@ -1,8 +1,35 @@
 package internal
 
 import (
+	"fmt"
 	"math/rand"
+	"regexp"
+	"strconv"
 )
+
+const (
+	gainPowerPattern = `gain (\d+) power`
+)
+
+var gainPowerRegex = regexp.MustCompile(gainPowerPattern)
+
+// actionionator
+func TakeAction(state State, action Action) State {
+
+	switch action.Name {
+	case gainPowerRegex.FindString(action.Name):
+		matches := gainPowerRegex.FindStringSubmatch(action.Name)
+		x, _ := strconv.Atoi(matches[1])
+		power := state.Villians["princejohn"].Power
+		power += x
+		villian := state.Villians["princejohn"]
+		villian.Power = power
+		state.Villians["princejohn"] = villian
+	default:
+		fmt.Println("Unhandled: " + action.Name)
+	}
+	return state
+}
 
 func ShuffleNTimes(cards []Card, n int) []Card {
 	for i := 0; i < n; i++ {
--- src/internal/engine_test.go
+++ src/internal/engine_test.go
@@ -7,15 +7,36 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestShuffle(t *testing.T){
+func TestShuffle(t *testing.T) {
 	locationFilePath, _ := filepath.Abs("../../data/pj_location.csv")
 	deckFilePath, _ := filepath.Abs("../../data/pj_deck.csv")
 
 	state := CreateGame(locationFilePath, deckFilePath)
-	deck := state.Villians["princejohn"].Deck 
+	deck := state.Villians["princejohn"].Deck
 	fourthCard := deck[3].Name
-	newDeck := ShuffleNTimes(deck,7)
+	newDeck := ShuffleNTimes(deck, 7)
 
 	// check that (random) 5th card has moved
 	assert.NotEqual(t, fourthCard, newDeck[3].Name)
+}
+
+func TestTakeTurn(t *testing.T) {
+	// WIP Mocking
+	locationFilePath, _ := filepath.Abs("../../data/pj_location.csv")
+	deckFilePath, _ := filepath.Abs("../../data/pj_deck.csv")
+
+	state := CreateGame(locationFilePath, deckFilePath)
+
+	for _, action := range state.Villians["princejohn"].Location["sherwood forest"].Actions {
+		state = TakeAction(state, action)
+	}
+	for _, action := range state.Villians["princejohn"].Location["friar tucks church"].Actions {
+		state = TakeAction(state, action)
+	}
+	for _, action := range state.Villians["princejohn"].Location["nottingham"].Actions {
+		state = TakeAction(state, action)
+	}
+	for _, action := range state.Villians["princejohn"].Location["the jail"].Actions {
+		state = TakeAction(state, action)
+	}
 }
